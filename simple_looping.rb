aeb2364e869c3b30777f496848f81e8331c8540e
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-22 16:21:13 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -54,7 +54,8 @@ end
 
 
 def for_iterator(number_of_times)
-  for number_of_times in 1..7 do
+  number_of_times = 1..7
+  for times in number_of_times do
     puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
