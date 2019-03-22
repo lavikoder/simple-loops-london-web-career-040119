e7fb11d2c22786cbf364d2475dedd3bfe077221d
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-22 16:16:58 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -42,9 +42,8 @@ while_iterator(7)
 
 def until_iterator(number_of_times)
   number_of_times = 0 
-  until number_of_times > 7 do
+  until number_of_times == 7 do
     puts "Welcome to Flatiron School's Web Development Course!"
-  
   end
 end
 
